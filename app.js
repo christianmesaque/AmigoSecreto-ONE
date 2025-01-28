e380ffe9fceffcf99546b839cdbd360e42d14f95
christianmesaque <christianmacedo110@gmail.com>
2025-01-27 22:02:20 -0300

    feat:sparkles: lógica concluída.

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+
+    if (nome == '') {
+        alert('Por favor, insira um nome.');
+    } else {
+        listaDeAmigos.push(nome);
+        console.log(listaDeAmigos);
+        limparCampo();
+        exibirListaDeAmigos();
+        listaDeAmigos
+    }
+}
+
+function exibirListaDeAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = listaDeAmigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
+
+function sortearAmigo() {
+    if (listaDeAmigos == '') {
+        alert('Por favor, insira um nome.')
+    } else {
+        const indiceDoGanhador = Math.floor(Math.random() * listaDeAmigos.length);
+        let ganhador = document.getElementById('resultado');
+        ganhador.innerHTML = `O amigo secreto sorteado é: ${listaDeAmigos[indiceDoGanhador]}`
+    }
+}
